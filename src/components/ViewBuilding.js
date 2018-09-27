d2c2934e9e7fd5aa908c6353b9aeb8a768e1fba4
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2018-09-27 00:17:12 -0400

    Enabled information from selected buildings to be displayed

--- src/components/ViewBuilding.js
+++ src/components/ViewBuilding.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
 class ViewBuilding extends React.Component {
+
 	render() {
-		return (
-			<div>
-				<p>
-					{' '}
-					<i>Click on a name to view more information</i>
-				</p>
-			</div>
-		);
+
+		const { data, selectedBuilding } = this.props;
+		//const bdgID = selectedBuilding.map( id =>{
+
+
+		//})
+
+		//if (this.props.selectedBuilding != 0){
+			//console.log('there was a building selected')
+			const viewBuilding = data
+			.filter(building => building.id === selectedBuilding).map(building =>{
+				return (
+
+							<li key={building.selectedBuilding}>
+							 <p>Name: </p>
+							 <p>{building.name}</p>
+							 <p>Address: </p>
+							 <p>{building.address}</p>
+
+							 </li>
+
+
+				)
+			})
+		//} else {
+			return (
+
+
+
+				<div className="selectedBuilding">
+					<p>
+						{' '}
+						<i>Click on a name to view more information</i>
+						<ul> {viewBuilding} </ul>
+					</p>
+				</div>
+			);
+	//}
 	}
 }
 export default ViewBuilding;
