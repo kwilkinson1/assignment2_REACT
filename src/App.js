29cc8faf2399f022ca3941af2a29175be5453aee
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2018-09-27 00:13:14 -0400

    Made app look more professional

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Search from './components/Search';
 import ViewBuilding from './components/ViewBuilding';
 import BuildingList from './components/BuildingList';
 import Credit from './components/Credit';
+import {render} from "react-dom";
+import logo from './logo-uf.svg';
+import bkgdImg from './uf-campus.jpg'
+//import logo-uf from './src/logo-uf.svg';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,21 +19,40 @@ class App extends React.Component {
 
   filterUpdate(value) {
     //Here you will need to set the filterText property of state to the value passed into this function
+    this.setState({
+      filterText: value
+
+    })
   }
 
   selectedUpdate(id) {
     //Here you will need to update the selectedBuilding property of state to the id passed into this function
+    this.setState({
+      selectedBuilding: id
+    })
+
   }
 
   render() {
-    
+
+
+
     return (
+
+
+
       <div className="bg">
-        <div className="row">
+        <div className="topRow">
+          <a href="www.ufl.edu">
+            <img src={logo} height="31" width="46" />
+        </a>
           <h1>UF Directory App</h1>
         </div>
 
-        <Search/>
+        <Search
+          filterText={this.state.filterText}
+          filterUpdate={this.filterUpdate.bind(this)}
+          />
         <main>
           <div className="row">
             <div className="column1">
@@ -42,17 +65,23 @@ class App extends React.Component {
                   </tr>
                   <BuildingList
                     data={this.props.data}
+                    filterText={this.state.filterText}
+                    selectedUpdate={this.selectedUpdate.bind(this)}
                   />
                 </table>
               </div>
             </div>
             <div className="column2">
-              <ViewBuilding />
+              <ViewBuilding
+                selectedBuilding={this.state.selectedBuilding}
+                data={this.props.data}
+              />
             </div>
           </div>
           <Credit />
         </main>
       </div>
+
     );
   }
 }
